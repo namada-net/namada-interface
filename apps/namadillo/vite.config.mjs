7d9ef9e2e630e30afeba316c1be0f4e534f89678
Mateusz Jasiuk <mateusz.jasiuk@gmail.com>
2025-04-29 16:06:13 +0200

    feat: osmosis calcs shiz wip

--- apps/namadillo/vite.config.mjs
+++ apps/namadillo/vite.config.mjs
@@ -3,7 +3,6 @@ import { sdkMulticoreWorkerHelpers } from "@namada/vite-esbuild-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
 import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -32,9 +31,6 @@ export default defineConfig(() => {
       }),
       react(),
       tsconfigPaths(),
-      nodePolyfills({
-        protocolImports: true,
-      }),
       checker({
         typescript: true,
         eslint: { lintCommand: 'eslint "./src/**/*.{ts,tsx}"' },
@@ -44,6 +40,12 @@ export default defineConfig(() => {
     worker: {
       plugins: () => [tsconfigPaths()],
       format: "es",
+    },
+    resolve: {
+      alias: {
+        buffer: "buffer",
+        process: "process/browser",
+      },
     },
     optimizeDeps: {
       esbuildOptions: {
