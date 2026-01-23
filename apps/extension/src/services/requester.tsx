ec245c28e2ebd54c12b421c83e2eda055b174fcf
H3ll0_Friend | 5ElementsNodes <100297125+h3ll0ofr1end@users.noreply.github.com>
2026-01-23 01:26:54 +0100

    Update requester.tsx

--- apps/extension/src/services/requester.tsx
+++ apps/extension/src/services/requester.tsx
@@ -22,8 +22,6 @@ const localStorage = new LocalStorage(
   new ExtensionKVStore(KVPrefix.LocalStorage, {
     get: area.get.bind(area),
     set: area.set.bind(area),
-    remove: area.remove.bind(area),
-    clear: area.clear.bind(area),
   })
 );
 const messenger = new ExtensionMessenger();
