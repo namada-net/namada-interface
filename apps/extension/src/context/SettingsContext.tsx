231147ef3dba72aaf4eede06c5432f3662e0cb45
H3ll0_Friend | 5ElementsNodes <100297125+h3ll0ofr1end@users.noreply.github.com>
2026-01-23 01:26:10 +0100

    Update SettingsContext.tsx

--- apps/extension/src/context/SettingsContext.tsx
+++ apps/extension/src/context/SettingsContext.tsx
@@ -11,8 +11,6 @@ const localStorage = new LocalStorage(
   new ExtensionKVStore(KVPrefix.LocalStorage, {
     get: area.get.bind(area),
     set: area.set.bind(area),
-    remove: area.remove.bind(area),
-    clear: area.clear.bind(area),
   })
 );
 
